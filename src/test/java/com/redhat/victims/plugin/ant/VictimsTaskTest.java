b8bd2b273e590099fcf9bb62bec7ffb4475dc81c
kgreav <kurt201@hotmail.com>
2013-07-14 19:45:13 +1000

    Modified tests to work with concurrent version

--- src/test/java/com/redhat/victims/plugin/ant/VictimsTaskTest.java
+++ src/test/java/com/redhat/victims/plugin/ant/VictimsTaskTest.java
@@ -12,87 +12,98 @@ import com.redhat.victims.VictimsException;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.types.Path;
-import org.apache.tools.ant.types.Resource;
-import org.apache.tools.ant.types.resources.FileProvider;
-import org.apache.tools.ant.types.resources.FileResource;
-import org.apache.tools.ant.util.ResourceUtils;
-import org.junit.Rule;
 import org.junit.Test;
 
 import com.redhat.victims.fingerprint.Metadata;
 
 public class VictimsTaskTest {
 
-    VictimsTask vt = new VictimsTask();
-    Project project = new Project();
-    
-    /**
-     * Lazy way to run the program
-     */
-    @Test
-    public void test() {
-        
-        vt.init();
-        Path path = new Path(project, "/home/kurt/ant/apache-ant-1.9.0/lib/*");
-        Path path2 = path.createPath();
-        vt.setPath(path2);
-        vt.execute();        
-    }
-    
-    /*
-     * Checks correct exception is thrown
-     */
-    @Test(expected=BuildException.class)
-    public void testVulnerabilityDetected() throws VictimsException {
-        String action = "fingerprint";
-        String cve    = "CVE-1111-1111";
-        Metadata meta = new Metadata();
-        vt.setMode("fatal");
-    //    vt.vulnerabilityDetected(action, meta, cve);
-    }
-    
-    @Test
-    public void testExecute(){
-    	vt.init();
-    	vt.setProject(project);
-        Path path = new Path(project, "");
-        Path path2 = path.createPath();
-        vt.setPath(path2);
-        /* assert default settings exist */
-        assertTrue(vt.getPath() != null);
-        assertTrue(!vt.getbaseUrl().equals(""));
-        assertTrue(!vt.getProject().equals(""));
-        assertTrue(vt.getUpdates().equalsIgnoreCase("auto")
-        			|| vt.getUpdates().equalsIgnoreCase("offline"));
-    }
-    
-    /**
-     * Needs jar with proper manifest info!
-     */
-    @Test
-    public void testMetadata(){
-    	vt.init();
-    	vt.setMode("fatal");
-    	File jar = new File("testdata","spring-2.5.6.jar");
-    	assertTrue(jar.canRead());
-    	try {
-    		Metadata meta = VictimsCommand.getMeta(jar);
-    		HashMap<String,String> gav = new HashMap<String,String>();
-    		if (meta.containsKey("Manifest-Version"))
-    			gav.put("groupId", meta.get("Manifest-Version"));
-    		if (meta.containsKey("Implementation-Version"))
-    			gav.put("artifactId", meta.get("Implementation-Version"));
-    		if (meta.containsKey("Implementation-Title"))
-    			gav.put("version", meta.get("Implementation-Title"));
-    		
-            assertTrue(gav.get("groupId").equals("1.0"));
-            assertTrue(gav.get("artifactId").equals("2.5.6"));
-            assertTrue(gav.get("version").equals("Spring Framework"));
-    	} catch (FileNotFoundException fn){
-    		
-    	} catch (IOException ie){
-    		
-    	}
-    }
+	VictimsTask vt = new VictimsTask();
+	Project project = new Project();
+
+	public void testScan() {
+
+	}
+
+	/**
+	 * Lazy way to run the program
+	 */
+	@Test
+	public void test() {
+
+		vt.init();
+		Path path = new Path(project, "/home/kurt/ant/apache-ant-1.9.0/lib/*");
+		Path path2 = path.createPath();
+		vt.setPath(path2);
+		vt.execute();
+	}
+
+	/**
+	 * Checks correct exception is thrown
+	 */
+	/*@Test(expected = BuildException.class)
+	public void testVulnerabilityDetected() throws VictimsException {
+		String action = "fingerprint";
+		String cve = "CVE-1111-1111";
+		Metadata meta = new Metadata();
+		// vt.vulnerabilityDetected(action, meta, cve);
+	}*/
+
+	/**
+	 * Test correct defaults
+	 */
+	@Test
+	public void testInit() {
+		vt.init();
+		System.out.println(vt.getbaseUrl());
+		vt.setProject(project);
+		Path path = new Path(project, "");
+		Path path2 = path.createPath();
+		vt.setPath(path2);
+		/* assert default settings exist */
+		assertTrue(vt.getPath() != null);
+		assertTrue(!vt.getbaseUrl().equals(""));
+		assertTrue(vt.getProject() != null);
+		String updates = vt.getUpdates();
+		assertTrue(updates.equalsIgnoreCase("auto")
+				|| updates.equalsIgnoreCase("offline")
+				|| updates.equalsIgnoreCase("daily"));
+		vt.setUpdates("auto");
+		assertTrue(vt.updatesEnabled());
+	}
+
+	/**
+	 * Tests retrieval of Jar manifest info
+	 * 
+	 * @throws VictimsException
+	 *             if test data unavailable
+	 */
+	@Test
+	public void testMetadata() throws VictimsException {
+		vt.init();
+		File jar = new File("testdata", "spring-2.5.6.jar");
+		if (!jar.canRead()) {
+			throw new VictimsException(
+					"Test data unavailable: spring-2.5.6.jar");
+		}
+		try {
+			Metadata meta = VictimsCommand.getMeta(jar);
+			HashMap<String, String> gav = new HashMap<String, String>();
+			if (meta.containsKey("Manifest-Version"))
+				gav.put("groupId", meta.get("Manifest-Version"));
+			if (meta.containsKey("Implementation-Version"))
+				gav.put("artifactId", meta.get("Implementation-Version"));
+			if (meta.containsKey("Implementation-Title"))
+				gav.put("version", meta.get("Implementation-Title"));
+
+			assertTrue(gav.get("groupId").equals("1.0"));
+			assertTrue(gav.get("artifactId").equals("2.5.6"));
+			assertTrue(gav.get("version").equals("Spring Framework"));
+		} catch (FileNotFoundException fn) {
+
+		} catch (IOException ie) {
+
+		}
+	}
 
 }
