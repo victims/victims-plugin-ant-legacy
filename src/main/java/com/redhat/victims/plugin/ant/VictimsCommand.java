896dd86c46c9140f3883495040fb5790d5ef99da
kgreav <kurt201@hotmail.com>
2013-07-20 19:26:53 +1000

    Fixes for metadata scanning

--- src/main/java/com/redhat/victims/plugin/ant/VictimsCommand.java
+++ src/main/java/com/redhat/victims/plugin/ant/VictimsCommand.java
@@ -21,17 +21,10 @@ package com.redhat.victims.plugin.ant;
  * #L%
  */
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.jar.Attributes;
 import java.util.concurrent.Callable;
-import java.util.jar.JarInputStream;
-import java.util.jar.Manifest;
-
-import org.apache.tools.ant.types.LogLevel;
 
 import com.redhat.victims.VictimsRecord;
 import com.redhat.victims.VictimsScanner;
@@ -68,42 +61,16 @@ public class VictimsCommand implements Callable<FileStub> {
 
 		// metadata
 		if (ctx.isEnabled(Settings.METADATA)){
-			Metadata meta = getMeta(jar.getFile());
-			// Create the VictimsRecord
-
             HashMap<String,String> gav = new HashMap<String,String>();
-            gav.put("groupId", meta.get("groupId"));
-            gav.put("artifactId", meta.get("artifactId"));
-            gav.put("version", meta.get("version"));
+            gav.put("title", jar.getTitle());
+            gav.put("artifactId", jar.getArtifactId());
+            gav.put("version", jar.getVersion());
             HashSet<String> cves = db.getVulnerabilities(gav);
             if (! cves.isEmpty()){
               throw new VulnerableDependencyException(jar, Settings.METADATA, cves);
 			}
 		}
 		return jar;
-	}
-
-	
-	/**
-	 * Creates metadata from a given jar file.
-	 * 
-	 * @param jar
-	 *            file containing a manifest
-	 * @return Metadata containing extracted information from manifest file.
-	 * @throws FileNotFoundException
-	 * @throws IOException
-	 */
-	public static Metadata getMeta(File jar) throws FileNotFoundException,
-			IOException {
-		if (!jar.getAbsolutePath().endsWith(".jar"))
-			return null;
-		JarInputStream jis = new JarInputStream(new FileInputStream(jar));
-		Manifest mf = jis.getManifest();
-		jis.close();
-		if (mf != null) {
-			return Metadata.fromManifest(mf);
-		}
-		return null;
 	}
 
 }
